6e4ace7665dceeb6c6a9883944d747e9997bddaf
rfabianf <rfabianf@everis.com>
2020-08-17 09:01:56 -0500

    BidirectionalBinding: Reflection y Arboles de Expresion

---
+++ BidirectionalObserver/BidirectionalBinding.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Text;
+
+namespace BidirectionalObserver
+{
+    public sealed class BidirectionalBinding : IDisposable
+    {
+        private bool disposed;
+
+        public BidirectionalBinding(
+            INotifyPropertyChanged first,
+            Expression<Func<object>> firstProperty,
+            INotifyPropertyChanged second,
+            Expression<Func<object>> secondProperty
+            )
+        {
+            // xxxProperty is MemberExpression
+            //Member ||| PropertyInfo
+            if(firstProperty.Body is MemberExpression firstExpr &&
+                secondProperty.Body is MemberExpression secondExpr)
+            {
+                if(firstExpr.Member is PropertyInfo firstProp &&
+                    secondExpr.Member is PropertyInfo secondProp)
+                {
+                    first.PropertyChanged += (sender, args) =>
+                     {
+                         if (!disposed)
+                             secondProp.SetValue(second, firstProp.GetValue(first));
+                     };
+
+                    second.PropertyChanged += (sender, args) =>
+                    {
+                        if (!disposed)
+                            firstProp.SetValue(first, secondProp.GetValue(second));
+                    };
+                }
+            }
+
+        }
+        public void Dispose()
+        {
+            disposed = true;
+        }
+    }
+}
--- BidirectionalObserver/Program.cs
+++ BidirectionalObserver/Program.cs
@@ -7,26 +7,27 @@
             Product product = new Product() { Name = "Iphone 5" };
             Window window = new Window() { ProductName = "Apple" };
 
-            product.PropertyChanged += (sender, eventArgs) =>
-                {
-                    if (eventArgs.PropertyName == "Name")
-                    {
-                        System.Console.WriteLine("Window.ProductName changed in Product");
-                        window.ProductName = product.Name;
-                    }
-            };
+            //product.PropertyChanged += (sender, eventArgs) =>
+            //    {
+            //        if (eventArgs.PropertyName == "Name")
+            //        {
+            //            System.Console.WriteLine("Window.ProductName changed in Product");
+            //            window.ProductName = product.Name;
+            //        }
+            //};
 
-            window.PropertyChanged += (sender, eventArgs) =>
-            {
-                if(eventArgs.PropertyName =="ProductName")
-                {
-                    System.Console.WriteLine("Product.Name changed in Window");
-                    product.Name = window.ProductName;
-                }
-            };
+            //window.PropertyChanged += (sender, eventArgs) =>
+            //{
+            //    if(eventArgs.PropertyName =="ProductName")
+            //    {
+            //        System.Console.WriteLine("Product.Name changed in Window");
+            //        product.Name = window.ProductName;
+            //    }
+            //};
 
-            product.Name = "Tablet";
-            //window.ProductName = "Lenovo I5";
+            var binding = new BidirectionalBinding(product, () => product.Name, window, () => window.ProductName);
+
+            product.Name = "Smart Tablet";
             System.Console.WriteLine(product);
             System.Console.WriteLine(window);
         }
