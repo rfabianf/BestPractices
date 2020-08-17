48666c26259483006ea2f1330d868d7a75af22b6
rfabianf <rfabianf@everis.com>
2020-08-17 00:40:39 -0500

    Patron Observador - Binding List -> Observable y Observador

--- ObservableCollections/Market.cs
+++ ObservableCollections/Market.cs
@@ -6,16 +6,37 @@ using System.Text;
 
 namespace ObservableCollections
 {
-    public class Market
+    public class Market //: INotifyPropertyChanged //Observable
     {
         public BindingList<float> prices = new BindingList<float>();
-
         public void AddPrice(float price)
         {
             prices.Add(price);
             //PriceAdded?.Invoke(this, price);
         }
 
-        public event EventHandler<float> PriceAdded;
+        //private float volatility;
+
+        //public float Volatility
+        //{
+        //    get => volatility;
+        //    set
+        //    {
+        //        if (value.Equals(volatility)) return;
+        //        volatility = value;
+        //        OnPropertyChanged ();
+        //    }
+        //}
+
+
+
+        //public event EventHandler<float> PriceAdded;
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
     }
 }
--- ObservableCollections/Program.cs
+++ ObservableCollections/Program.cs
@@ -3,40 +3,48 @@ using System.ComponentModel;
 
 namespace ObservableCollections
 {
-    class Program
+    internal class Program //Observador
     {
         static void Main(string[] args)
         {
             var market = new Market();
+
+            market.prices.ListChanged += (sender, eventArgs) =>
+            {
+                if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
+                {
+                    float price = ((BindingList<float>)sender)[eventArgs.NewIndex];
+                    Console.WriteLine($"We got a price of {price}");
+                }
+            };
+
+            market.AddPrice(200);
+            //market.PropertyChanged += (sender, EventArgs) =>
+            //{
+            //    if (EventArgs.Equals("volatility"))
+            //    {
+            //        Console.Write("prueba");
+            //    }
+            //};
+
+            //event Conventional
             //market.PriceAdded += (sender, f) =>
             //{
             //    Console.WriteLine($"We got a price of {f}");
             //};
-            //market.prices.ListChanged += (sender, eventArgs) =>
-            //{
-            //    if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
-            //    {
-            //        float price = ((BindingList<float>)sender)[eventArgs.NewIndex];
-            //        Console.WriteLine($"We got a price of {price}");
-            //    }
-            //};
+            //market.AddPrice(100);
 
-            market.prices.ListChanged += ListChangedTypes;
-
-            market.AddPrice(200);
-            market.AddPrice(100);
-
-            market.AddPrice(400);
+            //market.prices.ListChanged += ListChangedTypes;
         }
 
-        private static void ListChangedTypes(object sender, ListChangedEventArgs eventArgs)
-        {
-            if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
-            {
-                float price = ((BindingList<float>)sender)[eventArgs.NewIndex];
-                Console.WriteLine($"We got a price of {price}");
-            }
-        }
+        //private static void ListChangedTypes(object sender, ListChangedEventArgs eventArgs)
+        //{
+        //    if (eventArgs.ListChangedType == ListChangedType.ItemAdded)
+        //    {
+        //        float price = ((BindingList<float>)sender)[eventArgs.NewIndex];
+        //        Console.WriteLine($"We got a price of {price}");
+        //    }
+        //}
 
     }
 }
