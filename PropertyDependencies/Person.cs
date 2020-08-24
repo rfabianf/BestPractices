a0df5190948ddca5abb92685a92253aa08f3b33d
rfabianf <rfabianf@everis.com>
2020-08-23 22:56:37 -0500

    Property Dependencies - Observer Pattern

---
+++ PropertyDependencies/Person.cs
@@ -0,0 +1,80 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyDependencies
+{
+    public class Person : PropertyNotificationSupport
+    {
+       
+        public Person()
+        {
+            canVote = property(nameof(CanVote), () => Age >= 16 && Citizen);
+        }
+        private int age;
+        public int Age
+        { get => age;
+            set
+            {
+                //4->5
+                //false->false
+                //var oldCanVote = CanVote
+
+                if (value == age) return;
+                age = value;
+                OnPropertyChanged();
+                //OnPropertyChanged(nameof(CanVote));
+
+                //if(oldCanVote != CanVote)
+                //{
+                //    OnPropertyChanged(nameof(CanVote));
+                //}
+            }
+        }
+
+        private bool citizen;
+        public bool Citizen
+        {
+            get => citizen;
+            set
+            {
+                if (value == citizen) return;
+                citizen = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string sex;
+
+        public string Sex
+        {
+            get => sex;
+            set
+            {
+                if (value == sex) return;
+                sex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //public bool CanVote => Age >= 16;
+
+        //Utilizamos árboles de Expression
+        private readonly Func<bool> canVote;
+        public bool CanVote => canVote();
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+
+        //[NotifyPropertyChangedInvocator]
+        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+
+    }
+}
---
+++ PropertyDependencies/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyDependencies
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Person();
+            p.PropertyChanged += (sender, EventArgs) =>
+             {
+                 Console.WriteLine($"{EventArgs.PropertyName} changed");
+             };
+            p.Age = 15;
+            p.Citizen = true;
+            p.Sex = "M";
+        }
+    }
+}
