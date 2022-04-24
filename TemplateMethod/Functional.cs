b48d076cadf002cbfdb500a49ab90cbbe9c46c6f
Roberto <Everis>
2022-04-24 13:45:35 -0500

    Template Method

---
+++ TemplateMethod/Functional.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TemplateMethod
+{
+    public static class FunctionalTemplate
+    {
+        public static void Run(Action start,Action takeTurn, Func<bool> haveWinner, Func<int> winningPlayer)
+        {
+            start();
+            while (!haveWinner())
+                takeTurn();
+
+            Console.WriteLine($"Player {winningPlayer()} wins.");
+        }
+
+    }
+
+    public class Demo2
+    {
+        static void Main3(String[] args)
+        {
+            var numberOfPlayers = 3;
+            int currentPlayer = 0;
+            int turn = 1, maxTurns = 10;
+
+            void Start()
+            {
+                Console.WriteLine($"Starting a game of chess with {numberOfPlayers} players.");
+            }
+
+            bool haveWinner()
+            {
+                return turn == maxTurns;
+            }
+
+            void TakeTurn()
+            {
+                Console.WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+                currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+            }
+
+            int WinningPlayer()
+            {
+                return currentPlayer;
+            }
+
+            FunctionalTemplate.Run(Start, TakeTurn, haveWinner, WinningPlayer);
+        }
+    }
+}
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+
+    public abstract class Game
+    {
+        public void Run()
+        {
+            Start();
+            while (!HaveWinner)
+                TakeTurn();
+
+            Console.WriteLine($"Player {WinningPlayer} wins.");
+        }
+
+       
+        protected int currentPlayer;
+        protected readonly int numberofPlayers;
+
+        protected Game(int numberOfPlayers)
+        {
+            this.numberofPlayers = numberOfPlayers;
+
+        }
+
+        protected abstract void Start();
+        protected abstract void TakeTurn();
+        protected abstract bool HaveWinner { get; }
+        protected abstract int WinningPlayer { get; }
+
+    }
+
+
+    public class chess : Game
+    {
+        public chess(int numberOfPlayers) : base(numberOfPlayers)
+        {
+                
+        }
+        protected override bool HaveWinner => turn == maxTurns;
+
+        protected override int WinningPlayer => currentPlayer;
+
+        protected override void Start()
+        {
+            Console.WriteLine($"Starting a game of chess with {numberofPlayers} players");
+        }
+
+        protected override void TakeTurn()
+        {
+            Console.WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+            currentPlayer = (currentPlayer + 1) % numberofPlayers;
+        }
+
+        private int turn = 1;
+        private int maxTurns = 10;
+    }
+
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            var chess = new chess(3);
+            chess.Run();
+        }
+    }
+}
