19632ecfd40144c31969272de2d67dee12ce2105
Roberto <Everis>
2022-04-24 13:45:53 -0500

    Memento

---
+++ _Memento/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
